18a8becdb5f6f10f104e03f4cd37a0d1ab8fe9ff
JD2410 <jonathandelves@hotmail.com>
2025-07-04 04:47:46 -0400

    server settings

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<vjL5daxg1sOjPRUHHVmmpwfs>@172.21.255.21:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
