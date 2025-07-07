19d641d872cd6b621a920ac1ab6efcbdedd4856b
JD2410 <jonathandelves@hotmail.com>
2025-07-07 04:11:36 -0400

    Creating page components and basic routing

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:vjL5daxg1sOjPRUHHVmmpwfs@172.21.255.21:27017";
+const mongoURI =  "mongodb://root:vjL5daxg1sOjPRUHHVmmpwfs@172.21.96.187:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
