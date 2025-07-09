65893326c24c1598aee50abec181ae8551118ec3
JD2410 <jonathandelves@hotmail.com>
2025-07-09 06:03:15 -0400

    Find a doctor component styled

--- src/Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.js
+++ src/Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './FindDoctorSearchIC.css';
 import { useNavigate, Navigate } from 'react-router-dom';
-
+import doctorImage from './find_a_doctor.png';
 
 const initSpeciality = [
     'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
@@ -20,28 +20,31 @@ const FindDoctorSearchIC = () => {
     }
     return (
         <div className='finddoctor'>
-            <center>
-                <h1>Find a doctor and Consult instantly</h1>
-                <div>               <i style={{color:'#000000',fontSize:'20rem'}} className="fa fa-user-md"></i>
-</div>                <div className="home-search-container"  style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
-                    <div className="doctor-search-box">
-                    {/* <p>Perform a search to see the results.</p> */}
-
-                        <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
-                        
-                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt=""/></div>
-                        <div className="search-doctor-input-results" hidden={doctorResultHidden}>
-                            {
-                                specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
-                                    <span><img src={process.env.PUBLIC_URL + '/images/search.svg'} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
-                                    <span>{speciality}</span>
-                                    <span>SPECIALITY</span>
-                                </div>)
-                            }
-                        </div>
+            <div>
+                <img src={doctorImage} width="260px" height="260px" />
+            </div>
+            <div>
+            <h1>Find a doctor and Consult instantly</h1>
+                <p>Use the search below to find a specialty you need assistance with</p>
+            <div className="home-search-container"  style={{display:'flex',justifyContent:'center',alignItems:'center'}}> 
+                <div className="doctor-search-box">
+                {/* <p>Perform a search to see the results.</p> */}
+                    <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />  
+                    <i className="fa fa-search"></i>
+                    <div className="search-doctor-input-results" hidden={doctorResultHidden}>
+                        {
+                            specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
+                                <span><i className="fa fa-user"></i></span>
+                                <span>{speciality}</span>
+                                <span>SPECIALITY</span>
+                            </div>)
+                        }
                     </div>
                 </div>
-            </center>
+            </div>
+            </div>
+            
+            
         </div>
     )
 }
