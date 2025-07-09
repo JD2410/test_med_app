429953606dad1d9a18514cd1baf316e02943807e
JD2410 <jonathandelves@hotmail.com>
2025-07-09 03:31:35 -0400

    Consultation files and React popup added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up'
 import Login from './Components/Login/Login'
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'; 
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<Landing_page/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/instant-consultation" element={<InstantConsultation/>}/>
           </Routes>
         </BrowserRouter>
     </div>
