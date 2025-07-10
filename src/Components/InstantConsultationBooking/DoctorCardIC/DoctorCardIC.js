a659685ed158887b85ba5ef66028104bcd424812
JD2410 <jonathandelves@hotmail.com>
2025-07-10 04:31:18 -0400

    Doctors card created - reorder assets to make the accssible to mulitple componets - narrowed css to make it contained to component

--- src/Components/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.js
+++ src/Components/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.js
@@ -30,7 +30,7 @@ const DoctorCardIC = ({ name, speciality, experience, ratings, profilePic }) => 
   };
 
   return (
-    <div className="doctor-card-container">
+    <div className="doctor-card-container ic">
       <div className="doctor-card-details-container">
         <div className="doctor-card-profile-image-container">
         <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg>
