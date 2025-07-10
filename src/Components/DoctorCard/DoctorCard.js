5efb77a687b6090bab9075657a2074967cd92fd0
JD2410 <jonathandelves@hotmail.com>
2025-07-10 06:23:26 -0400

    Sorting out styling

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -28,7 +28,7 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
       setShowModal(false);
     };
   return (
-    <div className="doctor-card-container apointment">
+    <div className="doctor-card-container appointment">
       <div className="doctor-card-details-container">
         <div className="doctor-card-profile-image-container">
             {profilePic ? <img src={profilePic} /> : <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg>
